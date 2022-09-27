ba8c10ca331ca7e7a0b4d138c949fdc3d8f469c3
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-09-27 23:53:17 +0800

    up

--- js/scripts.js
+++ js/scripts.js
@@ -1,47 +1,47 @@
-function loadXMLDoc(documentName) {
-
-    'use strict';
-    const xmlhttp = new XMLHttpRequest();
-
-    xmlhttp.onreadystatechange = function() {
-
-      if (this.readyState == 4 && this.status == 200) {
-
-       displayResult(this);
-      }
-    };
-    xmlhttp.open("GET", "resources/classic-vehicles.xml", true);
-    xmlhttp.send();
-}
-
-
 function displayResult(xml) {
 
     'use strict';
     const xmlDoc = xml.responseXML;
     const result = xmlDoc.getElementsByTagName("vehicle");
-    var table = "<tr><th>name</th><th>Model</th><th>Country</th><th>Company</th><th>Price</th><th>Year</th></tr>";
+    var table = "";
 
     for (const item of result) {
-       
-        table += "<tr><td>" +
-       item.getElementsByTagName("name")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-       item.getElementsByTagName("model")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-       item.getElementsByTagName("country")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-       item.getElementsByTagName("company")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-       item.getElementsByTagName("price")[0].childNodes[0].nodeValue +
-        "</td><td>" +
-       item.getElementsByTagName("year")[0].childNodes[0].nodeValue +
-        "</td></tr>";
+
+        table += "<li> <ul> <li> <b> Name: \xa0 </b>" +
+
+            item.getElementsByTagName("name")[0].childNodes[0].nodeValue +
+            "</li> <li> <b> Model: \xa0 </b>" +
+            item.getElementsByTagName("model")[0].childNodes[0].nodeValue +
+            "</li> <li> <b> Country: \xa0 </b>" +
+            item.getElementsByTagName("country")[0].childNodes[0].nodeValue +
+            "</li> <li> <b> Model: \xa0 </b>" +
+            item.getElementsByTagName("company")[0].childNodes[0].nodeValue +
+            "</li> <li> <b> Company: \xa0 </b>" +
+            item.getElementsByTagName("price")[0].childNodes[0].nodeValue +
+            "</li> <li> <b> Year: \xa0 </b>" +
+            item.getElementsByTagName("year")[0].childNodes[0].nodeValue +
+
+        "</li> </ul> </li>";
     }
 
     document.getElementById("response").innerHTML = table;
 }
 
+function loadXMLDoc(documentName) {
+
+    'use strict';
+    const xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.onreadystatechange = function () {
+
+        if (this.readyState == 4 && this.status == 200) {
+
+            displayResult(this);
+        }
+    };
+    xmlhttp.open("GET", "resources/classic-vehicles.xml", true);
+    xmlhttp.send();
+}
 
 window.onload = function () {
 
