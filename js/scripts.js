cc631fe886e530cfc001bc1475878c7821532291
sunnysunny77 <46119793+sunnysunny77@users.noreply.github.com>
2022-09-28 19:07:10 +0800

    up

--- js/scripts.js
+++ js/scripts.js
@@ -1,49 +1,93 @@
-function displayResult(xml) {
+function events(obj, typ, callback, opts) {
 
     'use strict';
+
+    if (obj) {
+
+        obj.addEventListener(typ, callback, opts);
+    }
+}
+
+function result(xml) {
+
+    'use strict';
+
     const xmlDoc = xml.responseXML;
     const result = xmlDoc.getElementsByTagName("vehicle");
-    var table = "";
+    const pages = document.getElementById("pages");
+    const response = document.getElementById("response");
 
-    for (const item of result) {
+    let array = [];
+    let buffer = "";
+    let page = ""
 
-        const name = item.getElementsByTagName("name")[0].childNodes[0].nodeValue;
+    for (let i = 1; i <= result.length; i++) {
 
-        table += "<li> <ul class='vehicle'> <li>" + 
-        
-            "<img src='https://via.placeholder.com/200x200' alt='" + name + "'/>" +
+        const name = result[i - 1].getElementsByTagName("name")[0].childNodes[0].nodeValue;
+
+        buffer += "<li> <ul class='vehicle'> <li>" +
+
+            "<img src='./images/200x200.png' alt='" + name + "'/>" +
             "</li> <li> <b> Name: \xa0 </b>" +
             name +
             "</li> <li> <b> Model: \xa0 </b>" +
-            item.getElementsByTagName("model")[0].childNodes[0].nodeValue +
+            result[i - 1].getElementsByTagName("model")[0].childNodes[0].nodeValue +
             "</li> <li> <b> Country: \xa0 </b>" +
-            item.getElementsByTagName("country")[0].childNodes[0].nodeValue +
+            result[i - 1].getElementsByTagName("country")[0].childNodes[0].nodeValue +
             "</li> <li> <b> Model: \xa0 </b>" +
-            item.getElementsByTagName("company")[0].childNodes[0].nodeValue +
+            result[i - 1].getElementsByTagName("company")[0].childNodes[0].nodeValue +
             "</li> <li> <b> Company: \xa0 </b>" +
-            item.getElementsByTagName("price")[0].childNodes[0].nodeValue +
+            result[i - 1].getElementsByTagName("price")[0].childNodes[0].nodeValue +
             "</li> <li> <b> Year: \xa0 </b>" +
-            item.getElementsByTagName("year")[0].childNodes[0].nodeValue +
+            result[i - 1].getElementsByTagName("year")[0].childNodes[0].nodeValue +
 
-        "</li> </ul> </li>";
+            "</li> </ul> </li>";
+
+        if (i % 10 === 0 || i === result.length) {
+
+            array.push(buffer);
+            buffer = "";
+        }
     }
 
-    document.getElementById("response").innerHTML = table;
+    response.innerHTML = array[1];
+
+    for (let i = 1; i <= array.length; i++) {
+
+        page += "<span class='page'>" + [i] + "</span>";
+    }
+
+    pages.innerHTML = page;
+
+    const nodes = pages.children;
+
+    for (let i = 0; i <= nodes.length; i++) {
+
+        events(
+            nodes[i],
+            "click",
+            function () {
+                response.innerHTML = array[i]
+            },
+            null
+        );
+    }
 }
 
 function loadXMLDoc(documentName) {
 
     'use strict';
+
     const xmlhttp = new XMLHttpRequest();
 
     xmlhttp.onreadystatechange = function () {
 
         if (this.readyState == 4 && this.status == 200) {
 
-            displayResult(this);
+            result(this);
         }
     };
-    
+
     xmlhttp.open("GET", documentName, true);
     xmlhttp.send();
 }
@@ -51,5 +95,6 @@ function loadXMLDoc(documentName) {
 window.onload = function () {
 
     'use strict';
+
     loadXMLDoc("resources/classic-vehicles.xml");
 }
